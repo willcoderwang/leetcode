34ec9be10db6ebe23bb36f90dda3caa54cffcf44
王志坤 <wzk@disroot.org>
2020-07-29 07:55:48 +0800

    590

---
+++ 590.py
@@ -0,0 +1,24 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+
+        if not root.children:
+            return [root.val]
+
+        result = []
+        for child in root.children:
+            result.extend(self.postorder(child))
+
+        result.append(root.val)
+
+        return result
