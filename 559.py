1bfd9c027591767c637903a62bace6bc43f33468
王志坤 <wzk@disroot.org>
2021-07-19 21:16:25 +0800

    559

---
+++ 559.py
@@ -0,0 +1,21 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+
+        max_child_depth = 0
+        for child in root.children:
+            child_depth = self.maxDepth(child)
+            if child_depth > max_child_depth:
+                max_child_depth = child_depth
+
+        return max_child_depth + 1
