0e6b25fed223820fe105b651c6831b9b3885aebc
王志坤 <wzk@disroot.org>
2022-05-13 21:22:55 +0800

    21

--- 21.py
+++ 21.py
@@ -1,28 +1,19 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        if not list1:
-            return list2
-        if not list2:
-            return list1
 
-        new_head, new_tail = None, None
+        dummy = ListNode()
+        new_tail = dummy
 
         p1 = list1
         p2 = list2
 
         while p1 and p2:
             if p1.val <= p2.val:
-                if new_head:
-                    new_tail.next = p1
-                else:
-                    new_head = p1
+                new_tail.next = p1
                 new_tail = p1
                 p1 = p1.next
             else:
-                if new_head:
-                    new_tail.next = p2
-                else:
-                    new_head = p2
+                new_tail.next = p2
                 new_tail = p2
                 p2 = p2.next
 
@@ -31,4 +22,4 @@ class Solution:
         elif not p2:
             new_tail.next = p1
 
-        return new_head
+        return dummy.next
